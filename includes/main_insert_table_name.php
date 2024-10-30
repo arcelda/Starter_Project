52b0f182ee71eb48a4720c6030f894e875ca0ddf
arcelda <ajreyes@my.ccsu.edu>
2024-10-30 13:46:19 -0400

    first commit

---
+++ includes/main_insert_table_name.php
@@ -0,0 +1,16 @@
+<div class="container mt-3">
+    <h2>Table Name</h2>
+    <div class="card">
+        <div class="card-body">
+            <!--======================================================================-->
+            <form action="/action_page.php">
+                <div class="form-group">
+                    <label for="tabel_name">Table name:</label>
+                    <input type="name" class="form-control" id="table_name">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <!--======================================================================-->
+        </div>
+    </div>
+</div>
