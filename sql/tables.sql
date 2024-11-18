2ef888fa894024317ec21782ce7ffd24026e34ca
Alex J Reyes <arceldajactta@gmail.com>
2024-11-18 14:51:34 -0500

    Moved images table to tables sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -30,3 +30,10 @@ CREATE TABLE products (
   description varchar(255) DEFAULT NULL,
   PRIMARY KEY (product_id)
 );
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+);
