fc50c7880bce90eb8f2ac42929cc468e6a425a66
James Marshall <james.j.marshall@my.ccsu.edu>
2024-11-25 14:38:02 -0500

    nav recommend changes

--- includes/nav.php
+++ includes/nav.php
@@ -10,16 +10,17 @@
         <li><a href="video.php">Play Videos <i class="fas fa-video"></i></a></li>
         <li><a href="form_user.php">Add New User<i class="fas fa-file-alt"></i></a></li>
         <li><a href="display_users.php">Display Users <i class="fas fa-table"></i></a></li>
+        <!-- <li><a href="display_customers.php">Display Customers<i class="fas fa-table"></i></a></li> -->
 
-        <li><a href="get_last_insert_id.php">Get Last Insert ID <i class="fas fa-key"></i></a></li>
-        <li><a href="select_data.php">Select Data <i class="fas fa-search"></i></a></li>
-        <li><a href="select_data_into_datatable.php">Select Data Into Datatable <i class="fas fa-table"></i></a></li>
-        <li><a id="deleteDataButton" href="delete_data.php">Delete Data <i class="fas fa-trash"></i></a></li>
-        <li><a id="deleteDataButton" href="prepared_statements.php">Prepared Statements <i class="fas fa-code"></i></a></li>
-        <li><a id="deleteDataButton" href="limit_data.php">Limit Data <i class="fas fa-sliders-h"></i></a></li>
-        <li><a href="order_by.php">Order By <i class="fas fa-sort"></i></a></li>
-        <li><a href="update-data.php">Update Data <i class="fas fa-edit"></i></a></li>
-        <li><a href="Where.php">Where Statement <i class="fas fa-filter"></i></a></li>
-        <li><a href="display_users_with_images.php">Display Users with Images <i class="fas fa-table"></i></a></li>
+        <!-- <li><a href="get_last_insert_id.php">Get Last Insert ID <i class="fas fa-key"></i></a></li> -->
+        <!-- <li><a href="select_data.php">Select Data <i class="fas fa-search"></i></a></li> -->
+        <!-- <li><a href="select_data_into_datatable.php">Select Data Into Datatable <i class="fas fa-table"></i></a></li> -->
+        <!-- <li><a id="deleteDataButton" href="delete_data.php">Delete Data <i class="fas fa-trash"></i></a></li> -->
+        <!-- <li><a id="deleteDataButton" href="prepared_statements.php">Prepared Statements <i class="fas fa-code"></i></a></li> -->
+        <!-- <li><a id="deleteDataButton" href="limit_data.php">Limit Data <i class="fas fa-sliders-h"></i></a></li> -->
+        <!-- <li><a href="order_by.php">Order By <i class="fas fa-sort"></i></a></li> -->
+        <!-- <li><a href="update-data.php">Update Data <i class="fas fa-edit"></i></a></li> -->
+        <!-- <li><a href="Where.php">Where Statement <i class="fas fa-filter"></i></a></li> -->
+        <!-- <li><a href="display_users_with_images.php">Display Users with Images <i class="fas fa-table"></i></a></li> -->
     </ul>
 </nav>
