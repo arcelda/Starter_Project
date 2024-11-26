dea7ade0374efc122af8a93a1d2b909e0e7298a2
arcelda <arceldajactta@gmail.com>
2024-11-25 20:24:32 -0500

    Clean up

--- includes/nav.php
+++ includes/nav.php
@@ -7,8 +7,7 @@
         <li><a href="dashboard.php">Home <i class="fas fa-home"></i></a></li>
         <li><a href="insert_carousel_image.php">Insert Carousel Image <i class="fas fa-images"></i></a></li>
         <li><a href="audio.php">Play Audio <i class="fas fa-music"></i></a></li>
-        <li><a href="video.php">Play Videos <i class="fas fa-video"></i></a></li>
-        <li><a href="form_user.php">Add New User<i class="fas fa-file-alt"></i></a></li>
+        <li><a href="video.php">Play Videos <i class="fas fa-video"></i></a></li>    
         <li><a href="display_users.php">Display Users <i class="fas fa-table"></i></a></li>
         
         <!-- <li><a href="display_customers.php">Display Customers<i class="fas fa-table"></i></a></li> -->
