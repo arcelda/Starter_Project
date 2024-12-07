a6402320b2d1f8246fe3bc3dcf9827bed765d972
James Marshall <james.j.marshall@my.ccsu.edu>
2024-12-07 17:58:30 -0500

    updated lists

--- includes/nav.php
+++ includes/nav.php
@@ -8,7 +8,6 @@
         <!--<li><a href="carousel_images.php">Here are some inventory items! <i class="fas fa-shopping-cart"></i></a></li>-->
         <li><a href="audio.php">Play Audio <i class="fas fa-music"></i></a></li>
         <li><a href="video.php">Play Videos <i class="fas fa-video"></i></a></li>    
-        <li><a href="display_users.php">Display Users <i class="fas fa-table"></i></a></li>
         <li><a href="display_inventory.php">Display Inventory <i class="fas fa-table"></i></a></li>
 
     </ul>
