9cd227ea947363130b84fdd5807bf369c6be3c74
arcelda <arceldajactta@gmail.com>
2024-12-07 01:07:38 -0500

    I FUCKIN COOKED!

--- layer_presentation/productList.php
+++ layer_presentation/productList.php
@@ -16,11 +16,9 @@ $products = $controller->listProducts();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <!-- DataTables CSS -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-    <!-- Include DataTables CSS and jQuery -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+    <!-- DataTables CSS with Bootstrap styling -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+
 </head>
 
 <body>
@@ -71,16 +69,21 @@ $products = $controller->listProducts();
     <!-- Include jQuery and DataTables JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
     <script>
-    $(document).ready(function() {
-        $('#productTable').DataTable({
-            responsive: true,
-            autoWidth: false,
-            paging: true,
-            searching: true
-        });
+    $('#productTable').DataTable({
+        responsive: true,
+        autoWidth: false,
+        paging: true,
+        searching: true,
+        dom: '<"top"f>rt<"bottom"lp><"clear">',
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search products..."
+        }
     });
+
+
     </script>
 </body>
 
