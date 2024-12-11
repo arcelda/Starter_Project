6eddf06f2246c06f571507a41690256e80cfa407
Loobyb <loobybryan@gmail.com>
2024-12-11 13:21:34 -0500

    Fix

--- includes/process_registration.php
+++ includes/process_registration.php
@@ -4,7 +4,7 @@ $message = "";
 // Check if $conn is defined and available
 if (isset($conn)) {
     // Database name
-    $dbname = "myDBPDO";
+    $dbname = "mydbpdo";
 
     try {
         // Select the database
